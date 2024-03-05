4535b94d3e7050e61ca65a005fdf757dc74890ae
ayushkanani711 <ayushkanani2003@gmail.com>
2024-03-05 11:44:24 +0530

    Adding Vercel.json file

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/api", require("./route.js"));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Blinkit app!");
+  res.send("Welcome to the digital Calculator!");
 });
 
 app.listen(port, () => {
